3a90095283f1d75cec9d282b70955eaa2b8672d8
Luxshika Prashath <luxshikaprashath5@gmail.com>
2024-02-15 21:56:38 +0530

    Updated countchar function to exclude spaces

--- charcount.js
+++ charcount.js
@@ -1,8 +1,11 @@
 
-function countchar(){
+function countchar() {
   const GetString1 = document.querySelector('.paragraph');
   let paragraph = GetString1.value;
   
+  // Remove spaces from the string
+  paragraph = paragraph.replace(/\s/g, '');
+
   let length1 = paragraph.length;
-  document.querySelector('.answer').innerHTML = `<h1 class = "countchar">${length1}</h1>`;
+  document.querySelector('.answer').innerHTML = `<h1 class="countchar">${length1}</h1>`;
 }
